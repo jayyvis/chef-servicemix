503cf7f6d7c6465b7ff469d0b44c870d8b63c9ec
Mandeep <mandeep170@users.noreply.github.com>
2014-10-27 15:07:19 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures servicemix'
 license          'GPL v2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 supports         'rhel'
 
 depends "yum"
